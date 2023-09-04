46617884e6317838331dfe954a42b5cc882e868b
Nrulafind <mnurula.nu246@gmail.com>
2023-09-04 19:16:02 +0700

    prepare fot the controller testing code

--- app/controllers/test.controller.js
+++ app/controllers/test.controller.js
@@ -1,1 +1,45 @@
 const db = require("../models");
+const Test = db.test;
+const Op = db.Sequelize.Op;
+
+//Create
+exports.create = (req, res) => {
+
+
+
+
+};
+
+//Retrieve all data
+exports.findAll = (req, res) => {
+
+};
+
+//Find sigle data using id
+exports.findOne = (req, res) => {
+
+};
+
+//update sigle data using id
+exports.update = (req, res) => {
+
+
+};
+
+//delete sigle data using id
+exports.delete = (req, res) => {
+
+
+};
+
+//delete all sigle data
+exports.deleteAll = (req, res) => {
+
+
+};
+
+//find all data that has been published
+exports.finAllpublished = (req, res) => {
+
+
+};
