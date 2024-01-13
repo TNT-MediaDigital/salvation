a383441a9c62956450bb5df032d5b482dece0794
Nrulafind <mnurula.nu246@gmail.com>
2024-01-13 23:14:39 +0700

    re configure dotenv lib

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
 
 
 const app = express();
 app.use(cors());
-
 // Passport Configuration
 
 app.use(express.urlencoded({ extended: true }));
